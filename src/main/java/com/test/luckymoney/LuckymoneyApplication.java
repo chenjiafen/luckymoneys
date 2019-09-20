36ecc67f4443e9743ed45e5fccedb313fbc063ac
tyler <m_chenjiafeng@163.com>
2019-09-20 23:27:55 +0800

    项目启动注解

--- src/main/java/com/test/luckymoney/LuckymoneyApplication.java
+++ src/main/java/com/test/luckymoney/LuckymoneyApplication.java
@@ -3,7 +3,7 @@ package com.test.luckymoney;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //项目启动注解
 public class LuckymoneyApplication {
 
 	public static void main(String[] args) {
