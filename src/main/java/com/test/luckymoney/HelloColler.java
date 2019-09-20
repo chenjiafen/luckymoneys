9711cd63bfcb0657bfae4b8d454799402e75b4c5
tyler <m_chenjiafeng@163.com>
2019-09-21 00:21:15 +0800

    application.preoperties文件配置服务端口

--- src/main/java/com/test/luckymoney/HelloColler.java
+++ src/main/java/com/test/luckymoney/HelloColler.java
@@ -1,5 +1,6 @@
 package com.test.luckymoney;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloColler {
 
-        @RequestMapping(value = "/hello",method = RequestMethod.GET ) //请求接口，且是get方法
+//        @RequestMapping(value = "/hello",method = RequestMethod.GET ) //请求接口，且是get方法，旧版本
+        @GetMapping("/hello")
         public String say(){
             return "跟慕课老师学习SpringBoot";
         }
